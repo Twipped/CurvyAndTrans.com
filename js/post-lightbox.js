fdc46c2648877dd1f87ca16777be21ab6c94d8eb
Jocelyn Badgley (Twipped) <joc@twipped.com>
2019-04-02 11:08:07 -0700

    Adding magnific-popup for lightboxing

---
+++ js/post-lightbox.js
@@ -0,0 +1,26 @@
+(function ($) {
+
+  $('.lightbox').each(function () {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      closeOnContentClick: false,
+      closeBtnInside: false,
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true,
+      },
+      gallery: {
+        enabled: true,
+      },
+      zoom: {
+        enabled: true,
+        duration: 300, // don't foget to change the duration also in CSS
+        opener: function (element) {
+          return element.find('img');
+        },
+      },
+    });
+  });
+
+}(window.jQuery));
