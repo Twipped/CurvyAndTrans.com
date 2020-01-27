0e1438863dad3fdab5ca6d5d41f1eb448b897ff9
Jocelyn Badgley (Twipped) <joc@twipped.com>
2020-01-26 19:23:19 -0800

    Fix date bugs in gulp new

--- gulp/_template.js
+++ gulp/_template.js
@@ -1,9 +1,11 @@
 
+const format = require('date-fns/format');
+
 module.exports = exports = ({ id, date }) => `---
 id: "${id}"
 date: "${date.toISOString()}"
 title: ""
-description: "Outfit of the Day for ${date.format('MMM do, yyyy')}"
+description: "Outfit of the Day for ${format(date, 'MMM do, yyyy')}"
 tags:
   - OOTD
 products:
--- gulp/new.js
+++ gulp/new.js
@@ -1,6 +1,7 @@
 
 const argv = require('minimist')(process.argv.slice(2));
 const format = require('date-fns/format');
+const parse = require('date-fns/parse');
 const random = require('../lib/random');
 const path = require('path');
 const fs = require('fs-extra');
@@ -10,14 +11,15 @@ const template = require('./_template');
 const ROOT = path.dirname(__dirname);
 
 module.exports = exports = async function newPost () {
-  var date = new Date(argv.date || undefined);
+  var date = parse(argv.date, 'yyyy-MM-dd', new Date());
 
-  if (!date.hour()) {
+  if (!date.getHours()) {
     const now = new Date();
     date.setHours(now.getHours());
     date.setMinutes(now.getMinutes());
   }
 
+  // console.log(date);return;
   var id = random.id().substr(-6).toUpperCase();
   var fname = format(date, 'yyyy-MM-dd.HHmm.') + id;
 
