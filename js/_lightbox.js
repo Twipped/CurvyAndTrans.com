dd954b03c76001747d5a0279e75db02a0a230116
Jocelyn Badgley (Twipped) <joc@twipped.com>
2020-03-29 21:27:47 -0700

    New content for new engine

---
+++ js/_lightbox.js
@@ -0,0 +1,19 @@
+$(function () {
+
+  $('.lightbox, .tweet-entities').each(function () {
+    $(this).magnificPopup({
+      delegate: 'a.lb',
+      type: 'image',
+      closeOnContentClick: false,
+      closeBtnInside: false,
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true,
+      },
+      gallery: {
+        enabled: true,
+      },
+    });
+  });
+
+});
