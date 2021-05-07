e3a408f4df5660fe53ce2894aa5501ce976b89e8
Jocelyn Badgley (Twipped) <joc@twipped.com>
2021-05-07 14:17:56 -0700

    Updated timeline for 4 years

--- posts/2000-01-01.0003.DA0D14/#build.sh
+++ posts/2000-01-01.0003.DA0D14/#build.sh
@@ -1,2 +1,2 @@
 rm poster.jpeg
-montage -mode concatenate -background white -geometry 400x400^ -geometry +2+2 -gravity north -extent 400x500 -tile 6x6 *.jp* - | convert - -trim poster.jpeg
+montage -mode concatenate -background white -geometry 400x400^ -geometry +2+2 -gravity north -extent 400x500 -tile 12x4 *.jp* - | convert - -trim poster.jpeg
